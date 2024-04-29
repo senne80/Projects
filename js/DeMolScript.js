5d023dc4be8b402e92ca90ee7df232bdc3e3a36e
Senne Van Mol <senne.vanmol@student.hogent.com>
2024-04-29 22:34:49 +0200

    vraag implementatie

--- js/DeMolScript.js
+++ js/DeMolScript.js
@@ -6,16 +6,45 @@ const inpName = document.querySelector('#inpName');
 const test = document.querySelector('#test');
 
 // Spelers
-const players = ["Senne", "Nathalie", "Jonathan", "Wout", "Veerle", "Yves", "Admin", "Daan", "Gino"];
+const players = ["Senne", "Nathalie", "Jonathan", "Wout", "Veerle", "Yves", "Josie", "Daan", "Gino"];
+let activePlayer;
+
+// Questions
+const questionContainer = document.querySelector('.questionContainer');
 
 // Naam invullen en opslaan + naar volgende scherm
 btnNext.addEventListener('click', function() {
-    if (!players.includes(inpName.value))
+    if (players.includes(inpName.value))
     {
-        test.textContent = 'Gelieve je naam goed te schrijven';
+        if (inpName.value == 'Josie') {
+            test.textContent = 'Ej Josie stinkaard da werkt nog ni';
+            maakHomescreenLeeg();
+        }
+        else {
+            activePlayer = inpName.value;
+            test.textContent = `Proficiat ${activePlayer}, je bent ingelogd`;
+            maakHomescreenLeeg();
+        }
     }
     else
     {
-        test.textContent = 'U bent ingelogd';
+        test.textContent = 'Gelieve je naam goed te schrijven';
     }
 })
+
+function maakHomescreenLeeg() {
+    questionContainer.classList.remove('hidden')
+    lblName.classList.add('hidden');
+    btnNext.classList.add('hidden');
+    inpName.classList.add('hidden');
+}
+
+// Admin functies
+function startEliminatie() {
+
+}
+
+if (inpName.value == 'Josie')
+{
+    
+}
